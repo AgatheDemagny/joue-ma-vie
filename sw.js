23957abf7ff8a18220be3a0232e53d73fd8eb5ab
ady <71127699+AgatheDemagny@users.noreply.github.com>
2026-02-08 14:03:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker installé');
+const CACHE_NAME = "joue-ma-vie-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/js/worlds.js",
+  "/js/xp.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  // On peut ajouter un cache plus tard si besoin
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
